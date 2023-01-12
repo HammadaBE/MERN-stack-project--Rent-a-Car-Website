f42bf12c7f9dbdf251031ab60294b83d65129ed0
Legacy5618 <92302020+Legacy5618@users.noreply.github.com>
2023-01-12 15:15:59 +0000

    first commit

---
+++ Backend/config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.khalilocation.com',
+    'khalilocation.com'
+]
+
+module.exports = allowedOrigins
--- config/allowedOrigins.js
+++
@@ -1,7 +0,0 @@
-const allowedOrigins = [
-    'http://localhost:3000',
-    'https://www.khalilocation.com',
-    'khalilocation.com'
-]
-
-module.exports = allowedOrigins
